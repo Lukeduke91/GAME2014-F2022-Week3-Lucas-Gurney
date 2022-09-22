c46a249fe7def0fdca131904fd01ce30512650dc
Lukeduke91 <gurneylj@gmail.com>
2022-09-22 10:45:54 -0400

    Added screen touch

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -7,17 +7,19 @@ public class PlayerBehaviour : MonoBehaviour
     public float speed = 2.0f;
     public Boundary boundary;
     public float verticalPosition;
+    private Camera camera;
     // Start is called before the first frame update
 
     // Update is called once per frame
      void Start()
     {
-        
+        transform.position = new Vector2(0.0f, verticalPosition);
+        camera = Camera.main;
     }
     void Update()
     {
-        Move();
-
+        //Move();
+        GetMobileInput();
         //float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
         //float y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
 
@@ -27,12 +29,27 @@ public class PlayerBehaviour : MonoBehaviour
     public void Move()
     {
         float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
-
         transform.position += new Vector3(x, 0, 0);
+
         float clampedPosition = Mathf.Clamp(transform.position.x, boundary.min, boundary.max);
         transform.position = new Vector2(clampedPosition, verticalPosition);
     }
 
+    void GetMobileInput()
+    {
+        //Touch currentTouch = new Touch();
+
+        foreach (Touch touch in Input.touches) 
+        { 
+            //currentTouch = touch;
+            transform.position = camera.ScreenToWorldPoint(touch.position);
+        }
+        //var touch = Input.GetTouch(0);
+        
+
+        float clampedPosition = Mathf.Clamp(transform.position.x, boundary.min, boundary.max);
+        transform.position = new Vector2(clampedPosition, verticalPosition);
+    }
     //void CheckBounds()
     //{
     //    if (transform.position.x > boundary.max)
